6d1b430d57e1627cd5c5820a34ad2b06e386b7da
Greg Carr <gregcarrart@gmail.com>
2015-09-23 11:26:56 -0700

    adding uncompressed greenback libs

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,2 +1,10 @@
 //Main JS file
 var gQuery = require('libs/gquery/dist/gquery.min.js');
+var TweenLite = require('libs/TweenLite.js');
+var TimelineLite = require('libs/TimelineLite.js');
+var CSSPlugin = require('libs/CSSPlugin.js');
+var easePack = require('libs/EasePack.js');
+//Now you can use $('.class') or $('#id') selectors for
+//jQuery-like workflow. $.each(selector, callback), $.css({Object}),
+//$.on(eventType, callback). Greensock TweenLite and TimelineLite
+//are also included.

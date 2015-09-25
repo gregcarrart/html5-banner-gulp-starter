547f8a6ae8431364afb6ebd69e5579f96186055a
Greg Carr <gregcarrart@gmail.com>
2015-09-25 11:33:03 -0700

    adding some base style classes, removing easePack from main.js

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -3,8 +3,7 @@ var gQuery = require('libs/gquery/dist/gquery.min.js'),
     UTIL = require('./util.js'),
     TweenLite = require('libs/TweenLite.js'),
     TimelineLite = require('libs/TimelineLite.js'),
-    CSSPlugin = require('libs/CSSPlugin.js'),
-    easePack = require('libs/EasePack.js');
+    CSSPlugin = require('libs/CSSPlugin.js');
 //You can use $('.class').selector or $('#id').selector for
 //jQuery-like workflow. $.each(selector, callback), $.css({Object}),
 //$.on(eventType, callback). Greensock TweenLite and TimelineLite
@@ -12,10 +11,10 @@ var gQuery = require('libs/gquery/dist/gquery.min.js'),
 var Animations = (function () {
     'use strict';
 
-    var timeline,			// Main Animation that hold the child animations
-        variant,			// style... mpu / leaderboard etc
-        elements = {}, 		// Home of all of the DOM Node Elements
-        backgrounds = {};	// Just another handy container
+    var timeline,            // Main Animation that hold the child animations
+        variant,            // style... mpu / leaderboard etc
+        elements = {},         // Home of all of the DOM Node Elements
+        backgrounds = {};    // Just another handy container
 
     // Construct
     function Animations () {
@@ -24,58 +23,41 @@ var Animations = (function () {
 
     // This creates all of our animation elements
     Animations.prototype.assign = function () {
-        elements.ad     	= $('.ad').selector;
-        elements.first 		= $('#element-1').selector;
-        elements.second 	= $('#element-2').selector;
-        elements.third 		= $('#element-3').selector;
-        elements.fourth 	= $('#element-4').selector;
-        elements.cta 		= $('#cta').selector;
-        elements.logo 		= $('#logo').selector;
+        elements.ad                     = $('.ad').selector;
+        elements.frame1                 = $('#frame-1').selector;
+        elements.frame2                 = $('#frame-2').selector;
+        elements.frame3                 = $('#frame-3').selector;
+        elements.endframe               = $('#endframe').selector;
+        elements.first                  = $('#element-1').selector;
+        elements.second                 = $('#element-2').selector;
+        elements.third                  = $('#element-3').selector;
+        elements.fourth                 = $('#element-4').selector;
+        elements.cta                    = $('#cta').selector;
+        elements.logo                   = $('#logo').selector;
 
         this.construct();
     };
 
     // This creates all of our animation elements
     Animations.prototype.construct = function () {
-        var durationZoomIn 		= 1,
-            durationZoomOut 	= 1,
-            durationFadeIn 		= 1,
-            durationFadeOut 	= 1,
-            pauseDuration 		= 2;
+        var durationZoomIn      = 1,
+            durationZoomOut     = 1,
+            durationFadeIn      = 1,
+            durationFadeOut     = 1,
+            durationMove        = 0.5,
+            durationSwipe       = 0.6,
+            durationPan         = 4,
+            pauseDuration       = 2;
 
         // create a home for our animations
         // timeline = new TimelineLite( {onComplete:this.onFinish, onCompleteParams:["test1", "test2"], onCompleteScope:this } );
         timeline = new TimelineLite();
 
-        var scene1 = new TimelineLite();
+        var main = new TimelineLite();
         // Frame # 1 -------------------------------------------------------------------------
-        // Fade in
-        scene1.fromTo(elements.first, durationFadeIn, {autoAlpha:0}, {autoAlpha:1});
-        scene1.to(elements.first, durationFadeOut, {autoAlpha:0}, '+='+pauseDuration);
-
-        scene1.fromTo(elements.second, durationFadeIn, {autoAlpha:0}, {autoAlpha:1});
-        scene1.to(elements.second, durationFadeOut, {autoAlpha:0}, '+='+pauseDuration);
-
-        var scene2 = new TimelineLite();
-        // Frame # 2 -------------------------------------------------------------------------
-        // Fade in
-        scene2.fromTo(elements.third, durationFadeIn, {autoAlpha:0}, {autoAlpha:1});
-        scene2.to(elements.third, durationFadeOut, {autoAlpha:0}, '+='+pauseDuration);
-
-        scene2.fromTo(elements.fourth, durationFadeIn, {autoAlpha:0}, {autoAlpha:1});
-        scene2.to(elements.fourth, durationFadeOut, {autoAlpha:0}, '+='+pauseDuration);
-
-        var endframe = new TimelineLite();
-        // Endframe -------------------------------------------------------------------------
-        // Fade in
-        endframe.fromTo(elements.logo, durationFadeIn, {autoAlpha:0}, {autoAlpha:1});
-
-        endframe.fromTo(elements.cta, durationFadeIn, {autoAlpha:0}, {autoAlpha:1});
 
         // Create timeline
-        timeline.add(scene1);
-        timeline.add(scene2);
-        timeline.add(endframe);
+        timeline.add(main);
 
         // wait before starting the animation!
         timeline.pause();
@@ -101,4 +83,10 @@ window.onload = function () {
 
     anim.assign();
     anim.begin();
+
+    if (!UTIL.isMobile()) {
+        // Desktop-specific events
+    } else {
+        // Mobile Specific Events
+    }
 };
